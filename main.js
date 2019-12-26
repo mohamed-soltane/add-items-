e088014661f21a9a25dbe83de40d4c8cb02e5817
mohamed seifallah <soltanne92@gmail.com>
2019-12-26 06:33:36 -0600

    add items

---
+++ main.js
@@ -0,0 +1,47 @@
+
+/* eslint-disable */
+
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
+form.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+filter.addEventListener('keyup', filterItems);
+
+function addItem(e) {
+    'use strict';
+    e.preventDefault(e);
+    var newItem = document.getElementById('item').value;
+    var li = document.createElement('li');
+    li.className = "list-group-item";
+    li.appendChild(document.createTextNode(newItem));
+    var deleteBtn = document.createElement('button');
+    deleteBtn.className = "btn btn-danger btn-sm float-right delete";
+    deleteBtn.appendChild(document.createTextNode('X'));
+    li.appendChild(deleteBtn);
+    itemList.appendChild(li);
+}
+function removeItem(e) {
+        'use strict';
+   if(e.target.classList.contains('delete')) {
+        if(confirm('are you sure?')) {
+        var li = e.target.parentElement;
+        itemList.removeChild(li);
+    }
+}
+}
+function filterItems(e) {
+        'use strict';
+var text = e.target.value.toLowerCase();
+var items = itemList.getElementsByTagName('li');
+Array.from(items).forEach(function(item){
+        'use strict';
+var itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1) {
+        item.style.display = 'block';
+        } else {
+        item.style.display = 'none';
+    }                     
+    }
+)}
